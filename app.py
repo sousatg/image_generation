2e73c636c6114ac97eafdfbf3bacf99b258ecb86
Gil Sousa <tiagosousafl@gmail.com>
2022-11-27 07:40:49 +0100

    Generate a list of images

--- app.py
+++ app.py
@@ -1,30 +1,44 @@
-from PIL import Image, ImageDraw, ImageFont
 import csv
+from PIL import Image, ImageDraw, ImageFont
 from split_text import solution
+
+
+FONT = "Ubuntu-R.ttf"
+
+def get_font_size(im: Image, draw: ImageDraw, text: str) -> int:
+    """
+    Based in image size and the text calculate font size
+    to fill the image width
+    :param im Image
+    :param draw ImageDraw
+    :param text string
+    """
+    fontsize = 1
+    font = ImageFont.truetype(FONT, fontsize)
+
+    while draw.textsize(text, font)[0] < 0.9 * im.size[0]:
+        fontsize += 1
+        font = ImageFont.truetype(FONT, fontsize)
+
+    return fontsize
+
 
 with open('output/frases.csv', 'r') as fh:
     reader_csv = csv.reader(fh)
-    lines = next(reader_csv)
-    lines = next(reader_csv)[0]
+    count = 1
+    for row in reader_csv:
+        img = Image.new('RGB', (1080, 1080), '#F5D2B4')
+        draw = ImageDraw.Draw(img)
 
-img = Image.new('RGB', (1080, 1080), color = (73, 109, 137))
-text = solution("Amar é a sabedoria dos loucos e a loucura dos sábios").upper()
-fontsize = 1
-img_fraction = 0.90
+        text = solution(row[0].strip()).upper()
 
-draw = ImageDraw.Draw(img)
+        fontsize = get_font_size(img, draw, text) -1
 
-font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
-while font.getsize(text)[0] < img_fraction*img.size[0]:
-    fontsize += 1
-    font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
+        font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
 
-fontsize -= 1
-font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
+        w, h = draw.textsize(text, font=font)
 
-
-w, h = draw.textsize(text)
-
-draw.text(((1080-font.getsize(text)[0]) / 2,(1080-h) / 2), text, font=font, fill=(0,0,0))
- 
-img.save('pil_text.png')
+        draw.text(((1080-w) / 2,(1080-h) / 2),text=text, font=font, fill='#B46C63')
+            
+        img.save(f'images/img_{count}.png')
+        count +=1
--- test.py
+++ test.py
@@ -1,15 +1,37 @@
 from PIL import Image, ImageDraw, ImageFont
 from split_text import solution
-img = Image.new('RGB', (1080, 1080), color = (73, 109, 137))
 
-text = solution("Amar é a sabedoria dos loucos e a loucura dos sábios").upper()
 
-fontsize = 91 -1
+FONT = "Ubuntu-R.ttf"
 
+def get_font_size(im, draw, text):
+    """
+    Based in image size and the text calculate font size
+    to fill the image width
+    :param im Image
+    :param draw ImageDraw
+    : param text string
+    """
+    fontsize = 1
+    font = ImageFont.truetype(FONT, fontsize)
+
+    while draw.textsize(text, font)[0] < 0.9 * im.size[0]:
+        fontsize += 1
+        font = ImageFont.truetype(FONT, fontsize)
+
+    return fontsize
+
+img = Image.new('RGB', (1080, 1080), '#F5D2B4')
 draw = ImageDraw.Draw(img)
+
+text = solution("O amor está em quem ama e não em quem é amado. (Platão)").upper()
+
+fontsize = get_font_size(img, draw, text) -1
 
 font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
 
-draw.text((0,0),text=text, font=font, fill=(255,255,0))
+w, h = draw.textsize(text, font=font)
+
+draw.text(((1080-w) / 2,(1080-h) / 2),text=text, font=font, fill='#B46C63')
 
 img.save('test.png')
